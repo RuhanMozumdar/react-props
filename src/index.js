d42e05a9ed55b0e28c22f675f58d7a7d5fa131d8
ruhan <ruhanmozumdar369@gmail.com>
2023-05-10 22:57:24 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -4,10 +4,46 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function Card(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <img src={props.img} 
+      alt="avata_ img" />
+      <p>{props.tel}</p>
+      <p>{props.email}</p>
+    </div>
+  );
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <div>
+      <h1>My Contacts</h1>
+      <Card
+        name="Beyonce"
+        img="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
+        tel="+123 456 789"
+        email="b@beyonce.com"
+      />
+
+      <Card
+        name="Jack Bauer"
+        img="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
+        alt="avater_img"
+        tel="+987 654 321"
+        email="jack@nowhere.com"
+      />
+
+      <Card
+        name="Chuck Norris"
+        img="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
+        alt="avater_img"
+        tel="+918 372 574"
+        email="gmail@chucknorris.com"
+      />
+    </div>
   </React.StrictMode>
 );
 
